32080b7ccd404f41d2d032ebe474a3a7df0ab945
Saemi_hong <sstyle940104@gmail.com>
2022-01-14 18:18:32 +0900

    practice

---
+++ app.js
@@ -0,0 +1,63 @@
+// const container = document.getElementById('root');
+// const ajax = new XMLHttpRequest();
+// const content = document.createElement('div');
+// const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
+// const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
+
+// ajax.open('GET', NEWS_URL, false);
+// ajax.send();
+
+// const newsFeed = JSON.parse(ajax.response);
+// const ul = document.createElement('ul');
+
+// window.addEventListener('hashchange', function() {
+//   const id = location.hash.substr(1);
+
+//   ajax.open('GET', CONTENT_URL.replace('@id', id), false);
+//   ajax.send();
+
+//   const newsContent = JSON.parse(ajax.response);
+//   const title = document.createElement('h1');
+
+//   title.innerHTML = newsContent.title;
+
+//   content.appendChild(title);
+// });
+
+// for(let i = 0; i < 10; i++) {
+//   const li = document.createElement('li');
+//   const a = document.createElement('a');
+
+//   a.href = `#${newsFeed[i].id}`;
+//   a.innerHTML = `${newsFeed[i].title} (${newsFeed[i].comments_count})`;
+
+//   li.appendChild(a);
+//   ul.appendChild(li);
+// }
+
+// container.appendChild(ul);
+// container.appendChild(content);
+
+const ajax = new XMLHttpRequest();
+const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
+const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json' 
+ajax.open('GET',NEWS_URL, false )
+ajax.send()
+const res = ajax.response
+const results = JSON.parse(res)
+
+// 2. add click event using hashchange & location.hash.substring(1)
+window.addEventListener("hashchange", ()=>{
+  const id = location.hash.substring(1)
+  
+  ajax.open('GET', CONTENT_URL.replace("@id", id), false)
+  ajax.send();
+  console.log("hash");
+})
+// 1. add a href inside li
+const mapResult = results.map(item => `<li><a href=#${item.id}><h1>${item.title}</h1>(${item.comments_count})</a></li>`)
+
+
+document.getElementById("root").innerHTML = mapResult
+
+
